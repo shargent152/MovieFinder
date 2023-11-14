4dc44da41c418f91c5db5c17f8165c62e8448314
Trevor Peevers <shargent152@gmail.com>
2023-11-14 08:18:55 -0500

    fixed some problems

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.moviefinder2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.moviefinder2"
@@ -37,11 +37,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
--- app/src/main/java/com/example/moviefinder2/MainActivity.kt
+++ app/src/main/java/com/example/moviefinder2/MainActivity.kt
@@ -2,10 +2,97 @@ package com.example.moviefinder2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory.create
+import retrofit2.create
+var name = "asfda"
+var poster = ""
+var year = ""
+var rating = ""
+var length = ""
+var genre = ""
+var imdbrating = ""
+var imdburl = ""
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mainScreen = MainScreen()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.holder,mainScreen)
+            commit()
+        }
+        var viewModel: TitleViewModel = ViewModelProvider(this).get(TitleViewModel::class.java)
+        viewModel.selectedTitle.observe(this){
+            Log.i("bip","boop")
+            val key = "59f0f0ff"
+            Log.i("word",it)
+            val title = it
+            var interceptor : HttpLoggingInterceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+            var client : OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://www.omdbapi.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            val apiRunner = retrofit.create(MovieAPI::class.java)
+            val run = apiRunner.getMovie(title, key)
+            Log.i("maybe",client.toString())
+            run.enqueue(object : Callback<MovieDetails>{
+                override fun onResponse(
+                    call: Call<MovieDetails>,
+                    response: Response<MovieDetails>
+                ){
+                    if (response.isSuccessful){
+                        val movieDetails = response.body()
+                        Log.i("hope","test")
+                        Log.i("hope",response.body().toString())
+                        movieTime(movieDetails)
+                    }else{
+                        Toast.makeText(this@MainActivity, "Movie Not Found", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<MovieDetails>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, "WIFI has failed please try again later", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+    }
+    fun movieTime(movieDetails: MovieDetails?){
+        val selectedMovie = SelectedMovie()
+        val mainScreen = MainScreen()
+        movieDetails?.let {
+            name = it.Title
+            poster = it.Poster
+            year = it.Year
+            rating = it.Rated
+            length = it.Runtime
+            genre = it.Genre
+            imdbrating = it.imdbRating
+            imdburl = it.imdbID
+            Log.i("hello", title as String)
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.holder,selectedMovie)
+                commit()
+            }
+        }
     }
 }
+
