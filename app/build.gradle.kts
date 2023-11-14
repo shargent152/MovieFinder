66bf6ad620bec25120e1f03f718a5c03720f794d
Trevor Peevers <shargent152@gmail.com>
2023-11-14 17:11:28 -0500

    Fixed my emulator so that i could actually load the links without it crashing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
--- app/src/main/java/com/example/moviefinder2/MainActivity.kt
+++ app/src/main/java/com/example/moviefinder2/MainActivity.kt
@@ -3,32 +3,22 @@ package com.example.moviefinder2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
-import com.google.gson.Gson
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
-import retrofit2.CallAdapter
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.gson.GsonConverterFactory.create
-import retrofit2.create
-var name = "asfda"
-var poster = ""
-var year = ""
-var rating = ""
-var length = ""
-var genre = ""
-var imdbrating = ""
-var imdburl = ""
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var recycle : RecyclerView
+    private lateinit var manager : RecyclerView.LayoutManager
+    private lateinit var myAdapter: RecyclerView.Adapter<*>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,6 +27,7 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.holder,mainScreen)
             commit()
         }
+        manager = LinearLayoutManager(this)
         var viewModel: TitleViewModel = ViewModelProvider(this).get(TitleViewModel::class.java)
         viewModel.selectedTitle.observe(this){
             Log.i("bip","boop")
@@ -54,44 +45,30 @@ class MainActivity : AppCompatActivity() {
             val apiRunner = retrofit.create(MovieAPI::class.java)
             val run = apiRunner.getMovie(title, key)
             Log.i("maybe",client.toString())
-            run.enqueue(object : Callback<MovieDetails>{
+            run.enqueue(object : Callback<search>{
                 override fun onResponse(
-                    call: Call<MovieDetails>,
-                    response: Response<MovieDetails>
+                    call: Call<search>,
+                    response: Response<search>
                 ){
                     if (response.isSuccessful){
                         val movieDetails = response.body()
                         Log.i("hope","test")
                         Log.i("hope",response.body().toString())
-                        movieTime(movieDetails)
+                        recycle = findViewById<RecyclerView>(R.id.recycle).apply {
+                            myAdapter = SelectedMovie(response.body()!!)
+                            layoutManager = manager
+                            adapter = myAdapter
+                        }
+
                     }else{
                         Toast.makeText(this@MainActivity, "Movie Not Found", Toast.LENGTH_SHORT).show()
                     }
                 }
 
-                override fun onFailure(call: Call<MovieDetails>, t: Throwable) {
+                override fun onFailure(call: Call<search>, t: Throwable) {
                     Toast.makeText(this@MainActivity, "WIFI has failed please try again later", Toast.LENGTH_SHORT).show()
                 }
             })
-        }
-    }
-    fun movieTime(movieDetails: MovieDetails?){
-        val selectedMovie = SelectedMovie()
-        val mainScreen = MainScreen()
-        movieDetails?.let {
-            name = it.Title
-            poster = it.Poster
-            year = it.Year
-            rating = it.Rated
-            length = it.Runtime
-            genre = it.Genre
-            imdbrating = it.imdbRating
-            imdburl = it.imdbID
-            Log.i("hello", title as String)
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.holder,selectedMovie)
-                commit()
-            }
         }
     }
 }
